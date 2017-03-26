10cbe1d4727f073e06820d37629f904f531e7836
Max <tutv95@gmail.com>
2017-03-26 12:07:51 +0700

    Add libs

--- envatos/src/assets/js/options.js
+++ envatos/src/assets/js/options.js
@@ -1,3 +1,39 @@
-(function ($, chrome) {
-    console.log(chrome);
-})(jQuery, chrome);
+(function ($) {
+    var Model_Canned = Backbone.Model.extend({
+        initialize: function () {
+
+        },
+        defaults: {}
+    });
+
+    var Collection_Canned = Backbone.Collection.extend({
+        model: Model_Canned,
+        localStorage: new Backbone.LocalStorage("m_canned")
+    });
+
+    var View_List_Canned = Backbone.View.extend({
+        el: '#list-canned',
+
+        model: null,
+
+        initialize: function () {
+            this.model = new Collection_Canned();
+
+            this.render();
+        },
+
+        template: _.template($('#template-list-canned').html()),
+
+        render: function () {
+            this.$el.html(this.template(null));
+            return this;
+        }
+    });
+
+    $(document).ready(function () {
+        var list = new View_List_Canned();
+
+        console.log(list);
+    });
+
+})(jQuery);
